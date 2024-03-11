ccf2ee9684ef23c5d7e39abefba670c5d98948e9
Soprodevento <Sashinka2803@gmail.com>
2024-03-11 09:25:22 +0300

    исправил

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 9, 11, 24};
-        getSubArrayBetween(numbers, 1, 20);
-
+        int[] numbers = {1,2,3,4,5,6,7};
+        getSubArrayBetween(numbers,1,11);
     }
 
-    public static void getSubArrayBetween(int[] numbers, int start, int end) {
+    public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
         System.out.print("[");
         boolean firstNumberPrinted = false;
         for (int number : numbers) {
@@ -18,5 +17,6 @@ public class Main {
             }
         }
         System.out.print("]");
+        return numbers;
     }
 }
